ef5edc49a5d35f412787622c89f5bb9dcc7d62e0
DaoNamNghia <142570276+DaoNamNghia@users.noreply.github.com>
2023-12-10 17:08:18 +0700

    Update author.php

--- admin/author.php
+++ admin/author.php
@@ -20,7 +20,7 @@ if ($conn) {
     <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
     <div class="row">
         <div class="col-sm">
-            <a href="process_add_category.php" class="btn btn-success">Thêm mới</a>
+            <a href="process_add_author.php" class="btn btn-success">Thêm mới</a>
             <table class="table">
                 <thead>
                 <tr>
